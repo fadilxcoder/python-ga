867d286edf3a23cd1be86327124c8f56c5806e72
fadil-r <fadil.rosun@stonal.com>
2025-01-23 15:51:05 +0400

    using translator package

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello, world!")
+from translate import Translator
+
+translator = Translator(to_lang="French")
+
+translation = translator.translate('Hello!!! Welcome to my class')
+
+print(translation)
